4f18d01d236876aab53ac14a83426031f62d9367
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2018-01-11 12:21:12 -0600

    adempiere/LMX#16 Agregar complemento CFDI para donativos https://github.com/adempiere/LMX/issues/16

---
+++ sql/LMX_﻿Donatarias.sql
@@ -0,0 +1,17 @@
+﻿﻿--DROP VIEW LMX_Donatarias;
+CREATE OR REPLACE VIEW LMX_Donatarias AS
+SELECT 'es_MX' AS AD_Language,
+    ldt.AD_Client_ID,
+    ldt.AD_Org_ID,
+    ldt.CreatedBy,
+    ldt.Created,
+    ldt.UpdatedBy,
+    ldt.Updated ,
+    ldt.IsActive,
+    ldt.C_DocType_ID,
+    ldt.LMX_DocType_ID,
+    ldt.ConComplementoParaDonatarias,
+    TO_CHAR(ldt.fechaAutorizacion, 'yyyy-mm-dd"T"HH24:MI:SS ')  AS fechaAutorizacion,
+    ldt.noAutorizacion,
+    ldt.leyenda
+    FROM LMX_DocType ldt;
